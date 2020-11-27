ba20f643007bd67c92f1963b7c2fbbfc11b37a8a
shaankejriwal <70429914+shaankejriwal@users.noreply.github.com>
2020-11-27 12:09:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var spaceship,backgroundImg,background10;
+
+function preload(){
+ backgroundImg = loadImage("images/background.png");
+}
+
+function setup(){
+ createCanvas(windowWidth,windowHeight);
+ 
+ background10 = createSprite(width,height);
+ background10.addImage("images/background.png",backgroundImg);
+ background10.scale = 5;
+
+ background10.velocityY = 5;
+
+ spaceship = createSprite(width/2,height-100,30,30);
+}
+
+function draw(){
+ background(0);
+
+ if(background10.y>height){
+	background10.y = height/2;
+}
+
+ if(keyDown("UP_ARROW")){
+	 spaceship.y -= 10;
+ }
+
+ if(keyDown("DOWN_ARROW")){
+	 spaceship.y += 10;
+ }
+
+ if(keyDown("LEFT_ARROW")){
+	 spaceship.x -= 10;
+ }
+
+ if(keyDown("RIGHT_ARROW")){
+	 spaceship.x += 10;
+ }
+
+ drawSprites();
+}
+
+function spawnStars(){
+	for(var i=0;i<100;i++){
+
+	}
+}
